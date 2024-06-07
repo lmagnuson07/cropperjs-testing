0766eb1e133c130c2dd1d6a71d7c909dbd1c7ea3
Logan Magnuson <logmagns07@gmail.com>
2024-06-06 20:11:19 -0600

    Fixed an issue with duplicate boxes on file upload. Got file upload and preview working.

--- js/main.js
+++ js/main.js
@@ -1,19 +1,53 @@
-// initialize jquery-cropperjs
+// // initialize jquery-cropperjs
 $(document).ready(function() {
 // Import image
+    const console = window.console || { log: function () {} };
+    const URL = window.URL || window.webkitURL;
     let uploadedImageName;
     let uploadedImageURL;
     let uploadedImageType;
     let $inputImage = $('#inputImage');
+    let $image = $('#image');
+
+    var options = {
+        // aspectRatio: 16 / 9,
+        // preview: '.img-preview',
+        // crop: function (e) {
+        // }
+    };
+
+    // Cropper
+    $image.on({
+        ready: function (e) {
+            console.log(e.type);
+        },
+        cropstart: function (e) {
+            console.log(e.type, e.detail.action);
+        },
+        cropmove: function (e) {
+            console.log(e.type, e.detail.action);
+        },
+        cropend: function (e) {
+            console.log(e.type, e.detail.action);
+        },
+        crop: function (e) {
+            console.log(e.type);
+        },
+        zoom: function (e) {
+            console.log(e.type, e.detail.ratio);
+        }
+    }).cropper(options);
+
+    console.log($image);
 
     if (URL) {
         $inputImage.change(function () {
             let files = $(this)[0].files;
             let file;
 
-            // if (!$image.data('cropper')) {
-            //     return;
-            // }
+            if (!$image.data('cropper')) {
+                return;
+            }
 
             if (files && files.length) {
                 file = files[0];
@@ -25,9 +59,8 @@ $(document).ready(function() {
                     if (uploadedImageURL) {
                         URL.revokeObjectURL(uploadedImageURL);
                     }
-
                     uploadedImageURL = URL.createObjectURL(file);
-                    // $image.cropper('destroy').attr('src', uploadedImageURL).cropper(options);
+                    $image.cropper('destroy').attr('src', uploadedImageURL).cropper(options);
                     $inputImage.val('');
                 } else {
                     window.alert('Please choose an image file.');
@@ -39,4 +72,3 @@ $(document).ready(function() {
         $inputImage.prop('disabled', true).parent().addClass('disabled');
     }
 });
-
