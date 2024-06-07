0dd8a12168c239513cb36657bd3def4b54e1260d
Logan Magnuson <logmagns07@gmail.com>
2024-06-06 21:46:34 -0600

    Added zoom and move buttons.

--- js/main.js
+++ js/main.js
@@ -8,6 +8,7 @@ $(document).ready(function() {
     let uploadedImageType;
     let $inputImage = $('#inputImage');
     let $image = $('#image');
+    let originalImageURL = $image.attr('src')
 
     var options = {
         dragMode: 'move',
@@ -46,6 +47,104 @@ $(document).ready(function() {
     }).cropper(options);
 
     console.log($image);
+
+    // Methods
+    $('.cropper-console').on('click', '[data-method]', function () {
+        var $this = $(this);
+        var data = $this.data();
+        var cropper = $image.data('cropper');
+        var cropped;
+        var $target;
+        var result;
+
+        if ($this.prop('disabled') || $this.hasClass('disabled')) {
+            return;
+        }
+
+        if (cropper && data.method) {
+            data = $.extend({}, data); // Clone a new one
+
+            if (typeof data.target !== 'undefined') {
+                $target = $(data.target);
+
+                if (typeof data.option === 'undefined') {
+                    try {
+                        data.option = JSON.parse($target.val());
+                    } catch (e) {
+                        console.log(e.message);
+                    }
+                }
+            }
+            console.log(data);
+            // cropped = cropper.cropped;
+            //
+            // switch (data.method) {
+            //     case 'rotate':
+            //         if (cropped && options.viewMode > 0) {
+            //             $image.cropper('clear');
+            //         }
+            //
+            //         break;
+            //
+            //     case 'getCroppedCanvas':
+            //         if (uploadedImageType === 'image/jpeg') {
+            //             if (!data.option) {
+            //                 data.option = {};
+            //             }
+            //
+            //             data.option.fillColor = '#fff';
+            //         }
+            //
+            //         break;
+            // }
+
+            result = $image.cropper(data.method, data.option, data.secondOption);
+
+            // switch (data.method) {
+            //     case 'rotate':
+            //         if (cropped && options.viewMode > 0) {
+            //             $image.cropper('crop');
+            //         }
+            //
+            //         break;
+            //
+            //     case 'scaleX':
+            //     case 'scaleY':
+            //         $(this).data('option', -data.option);
+            //         break;
+            //
+            //     case 'getCroppedCanvas':
+            //         if (result) {
+            //             // Bootstrap's Modal
+            //             $('#getCroppedCanvasModal').modal().find('.modal-body').html(result);
+            //
+            //             // if (!$download.hasClass('disabled')) {
+            //             //     download.download = uploadedImageName;
+            //             //     $download.attr('href', result.toDataURL(uploadedImageType));
+            //             // }
+            //         }
+            //
+            //         break;
+            //
+            //     case 'destroy':
+            //         if (uploadedImageURL) {
+            //             URL.revokeObjectURL(uploadedImageURL);
+            //             uploadedImageURL = '';
+            //             $image.attr('src', originalImageURL);
+            //         }
+            //
+            //         break;
+            // }
+
+            if ($.isPlainObject(result) && $target) {
+                try {
+                    $target.val(JSON.stringify(result));
+                } catch (e) {
+                    console.log(e.message);
+                }
+            }
+        }
+    });
 
     if (URL) {
         $inputImage.change(function () {
